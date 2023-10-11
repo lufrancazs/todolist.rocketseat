1054a8dfc1ed230b99e3755bc3cd75c4489856a7
Lucas Martins <lucasfranca_zs@hotmail.com>
2023-10-11 15:59:32 -0300

    Primeira e Segunda Aula de Java - RocketSeat

---
+++ todolist/src/main/java/br/com/lucasfranca/todolist/user/UserModel.java
@@ -0,0 +1,81 @@
+package br.com.lucasfranca.todolist.user;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity(name = "tb_users")
+public class UserModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    @Column(unique = true)
+    private String username;
+    private String name;
+    private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+
+    public UserModel() {
+
+    }
+
+    public UserModel(String username, String name, String password, UUID id, LocalDateTime createdAt){
+        this.username = username;
+        this.name = name;
+        this.password = password;
+        this.id = id;
+        this.createdAt = createdAt;
+    }
+
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public void setUserName(String username) {
+        this.name = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
