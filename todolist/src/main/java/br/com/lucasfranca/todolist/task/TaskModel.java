6b31ebfeca18924e3c7f9ba8e06d000f05f931d3
Lucas Martins <lucasfranca_zs@hotmail.com>
2023-10-12 19:04:25 -0300

    Implementado segurança nos dados do usuário

---
+++ todolist/src/main/java/br/com/lucasfranca/todolist/task/TaskModel.java
@@ -0,0 +1,117 @@
+package br.com.lucasfranca.todolist.task;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity(name = "tb_tasks")
+public class TaskModel {
+    
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+    private String description;
+
+    @Column(length = 50)
+    private String title;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
+    private String priority;
+    private UUID idUser;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    
+
+
+
+public TaskModel() {
+
+    }
+
+public TaskModel(UUID id, String description, String title, LocalDateTime startAt, LocalDateTime endAt, String priority, LocalDateTime createdAt, UUID idUser)  {
+    this.id = id;
+    this.description = description;
+    this.title = title;
+    this.startAt = startAt;
+    this.endAt = endAt;
+    this.priority = priority;
+    this.createdAt = createdAt;
+    this.idUser = idUser;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setEndAt(LocalDateTime endAt) {
+        this.endAt = endAt;
+    }
+
+    public LocalDateTime getEndAt() {
+        return endAt;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setIdUser(UUID idUser) {
+        this.idUser = idUser;
+    }
+
+    public UUID getIdUser() {
+        return idUser;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setStartAt(LocalDateTime startAt) {
+        this.startAt = startAt;
+    }
+
+    public LocalDateTime getStartAt() {
+        return startAt;
+    }
+    
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    
+
+
+}
